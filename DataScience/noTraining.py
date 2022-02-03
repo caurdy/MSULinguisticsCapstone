3349cac8705dac34930c24de4b915d1fb08dad1b
Jacob Caurdy <caurdyja@msu.edu>
2022-02-02 20:26:58 -0500

    initial commit

--- DataScience/noTraining.py
+++ DataScience/noTraining.py
@@ -10,11 +10,11 @@ from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 #file_name = "./Data/wav/0a3bec98-a24f-4e17-b03c-9f4b662fb469.wav"
 #file_name = "./Data/wav/47b68b9a-48c2-419f-8adb-60e116b545e5"
 # Russ <10 sec clip
-file_name = r"C:\Users\caurd\Desktop\MSU\Spring '22\CSE 498\CapstoneCode\Data\wav\cea2c7ae-f6e7-40f7-b992-979bfd41771c.wav"
+file_name = r"/Data/wav/cea2c7ae-f6e7-40f7-b992-979bfd41771c.wav"
 # 30 sec clip - male college
-file_name2 = r"C:\Users\caurd\Desktop\MSU\Spring '22\CSE 498\CapstoneCode\Data\wav\780cbc91-6131-41f0-9088-a3ddae83877c.wav"
+file_name2 = r"/Data/wav/780cbc91-6131-41f0-9088-a3ddae83877c.wav"
 # 1 min clip - female 16-24 age range
-file_name3 = r"C:\Users\caurd\Desktop\MSU\Spring '22\CSE 498\CapstoneCode\Data\wav\c3fc8885-6500-4203-9cd0-03771b001a91.wav"
+file_name3 = r"/Data/wav/c3fc8885-6500-4203-9cd0-03771b001a91.wav"
 
 # Facebooks non-fine tuned version of wav2vec2 trained on 960 hours of transcribed speech
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
