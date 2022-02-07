57247979c67bbd53a330b36e1b96d8a000055922
Jacob Caurdy <caurdyja@msu.edu>
2022-02-06 21:26:42 -0500

    V1.0 FineTuning Wav2Vec2 FineTuning script working for training (no prediction or saving) Data engineering scripts (cleaning/formatting data and saving to .json)
    Added requirements.txt

--- DataEngineering/dataConfig.py
+++ DataEngineering/dataConfig.py
@@ -1,20 +1,31 @@
 """
-Reconfiguring
+Data Engineering dataConfig.py
+Functions for cleaning and prepping the data for training
 """
 
-import pandas as pd
 import os
-import librosa
-
+import re
+import json
+from librosa.core import load
+import pandas as pd
 
 # ToDo
-# Account for corrected data transcripts
+#  Add parameter for file size convertToDataframe
+
+# Notes
+# Special Characters in transcripts:
+#   How do we handle numeric characters? For now I'm removing them but how does the transcript handle them?
+#   what about +,-,/,*?
 #
 
-def convertToDataframe(transcript_dir: str = '../Data/Transcripts', audio_dir: str = '../Data/wav') -> pd.DataFrame():
+chars_to_ignore_regex = r'[\d\,\?\.\!\-\;\:\"\}\{\©\[\]\)\(\+\-\*\¼\%]'
+
+
+def convertToDataframe(transcript_dir: str = '../Data/Transcripts/', audio_dir: str = '../Data/wav/') -> pd.DataFrame():
     """
     Convert a directory of transcripts and audios to a single pandas dataframe
-    columns are 'index, id (filename), transcript, audio_file_path
+    This version currently loads corrected transcripts only
+    columns are [file, text, audio, sampling_rate]
     Assumptions:
         The transcript directory has headers seperated by tabs in the order:
             Speaker, Header2, Chunk_start, Chunk_End, Chunk
@@ -24,26 +35,65 @@ def convertToDataframe(transcript_dir: str = '../Data/Transcripts', audio_dir: s
     :return: Pandas Dataframe
     """
 
-    df = pd.DataFrame()
+    df = pd.DataFrame(columns=['file', 'text', 'audio', 'sampling_rate'])
 
     # Read transcripts into dataframe
     for filename in os.listdir(transcript_dir):
+        if 'corrected' not in filename:
+            continue
+
+        path = os.path.join(transcript_dir, filename)
+        if os.path.getsize(path) > 1000:  # skip files over 10KB
+            continue
+
         data = {'file': filename}
-        with open(os.path.join(transcript_dir, filename)) as fp:
+        with open(path) as fp:
             fp.readline()  # skip past header
+            transcript = ""
             for line in fp.readlines():
-                transcript = line.split('\t')[-1]  # get the last split assumed to be the text
-                data['text'] = transcript
+                transcript += line.split('\t')[-1].replace('\n', ' ')  # get the last split assumed to be the text
+            data['text'] = transcript
 
+        filename = filename.replace('-corrected', '')
         filename = filename.split('.')[0] + '.wav'
-        try:
-            with open(os.path.join(audio_dir, filename)) as fp:
-                data['audio'] = None
-                pass  # do stuff with audio file
+        audio_path = os.path.join(audio_dir, filename)
+        audio_array, sampling_rate = load(audio_path, sr=16000)  # files loaded at 22050 SR for some reason by default
+        data['audio'] = audio_array
+        data['sampling_rate'] = sampling_rate
 
-        except FileNotFoundError as e:
-            pass
+        df.loc[len(df.index)] = data
+
+    return df
+
+
+def remove_special_characters(batch):
+    return re.sub(chars_to_ignore_regex, '', batch).lower() + " "
+
+
+def extract_all_chars(batch):
+    all_text = " ".join(batch)
+    vocab = list(set(all_text))
+    return {"vocab": [vocab], "all_text": [all_text]}
+
+
+def createVocabulary(series: pd.Series) -> dict[str: int]:
+    text_list = series.tolist()
+    textBlob = " ".join(text_list)
+    vocabList = set(textBlob)
+    vocab_dict = {v: k for k, v in enumerate(vocabList)}
+    vocab_dict['|'] = vocab_dict[' ']
+    del vocab_dict[' ']
+    try:
+        del vocab_dict['ã']  # where tf is this coming from?
+    except KeyError as e:
+        print('Key error')
+    vocab_dict["[UNK]"] = len(vocab_dict)
+    vocab_dict["[PAD]"] = len(vocab_dict)
+    return vocab_dict
 
 
 if __name__ == '__main__':
-    convertToDataframe()
+    df = convertToDataframe()
+    df['text'] = df['text'].apply(remove_special_characters)
+    # vocab = createVocabulary(df['text'])
+    df.to_json('../Data/corrected.json')
--- DataEngineering/dataExploration.py
+++ DataEngineering/dataExploration.py
@@ -4,11 +4,34 @@ Running analysis on transcript file from directory 'MCD transcripts - tsv 2022-0
 import os
 import shutil
 
-oldDir = './Data/MCD transcripts - tsv 2022-01-17-1'
-newDir = './Data/Transcripts'
-for filename in os.listdir(oldDir):
-    if filename[0] != '.':
-        shutil.copy(os.path.join(oldDir, filename), newDir)
+oldDir = '../Data/MCD transcripts - tsv 2022-01-17-1'
+newDir = '../Data/Transcripts'
+
+def cleanTranscripts():
+    for filename in os.listdir(oldDir):
+        if filename[0] != '.':
+            shutil.copy(os.path.join(oldDir, filename), newDir)
+
+
+def transcriptStatistics():
+    count = 0
+    for filename in os.listdir(newDir):
+        if 'correct' in filename:
+            count += 1
+    print('Number of corrected transcripts', count)
+
+
+def sizeStatistics():
+    count = 0
+    total_size = 0
+    for filename in os.listdir(newDir):
+        path = os.path.join(newDir, filename)
+        size = os.path.getsize(path)
+        if 'correct' in filename:
+            total_size += size
+            count += 1
+    print('Average size (bytes): ', total_size/count)
+
 
 if __name__ == "__main__":
-    pass
+    sizeStatistics()
